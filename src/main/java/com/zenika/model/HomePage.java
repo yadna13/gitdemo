d5726e94852a305aff0e66f24721d2e28a58756b
Nagen <nagendra.yadav.niit@gmail.com>
2016-06-14 14:41:30 -0700

    asd

---
+++ src/main/java/com/zenika/model/HomePage.java
@@ -0,0 +1,42 @@
+package com.zenika.model;
+
+/**
+ * 
+ * @author VC1
+ *
+ */
+public class HomePage {
+	private String name;
+	private String email;
+	private String mobile;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getMobile() {
+		return mobile;
+	}
+
+	public void setMobile(String mobile) {
+		this.mobile = mobile;
+	}
+
+	@Override
+	public String toString() {
+		return "HomePage [name=" + name + ", email=" + email + ", mobile=" + mobile + "]";
+	}
+
+}
